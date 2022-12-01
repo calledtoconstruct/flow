094dc032ea73ea72be7f72dc69158bc0a3959d0b
Joseph Woolley <joe@calledtoconstruct.net>
2022-12-01 07:59:10 -0500

    Add .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# Flow" 
+# Flow
+
+A collection of potentially useful classes for streamlining the process flow of Java applications.
--- flow/src/main/java/net/calledtoconstruct/App.java
+++ flow/src/main/java/net/calledtoconstruct/App.java
@@ -3,10 +3,6 @@ package net.calledtoconstruct;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) throws InterruptedException, ExecutionException
